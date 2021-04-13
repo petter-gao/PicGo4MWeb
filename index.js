dff982aabf94656c2813cabe75e633b3fb5f0868
高鹏 <gaop@gratone.cn>
2021-04-14 06:50:44 +0800

    支持批量上传
    gitee-uploader 有点问题,时不时会发生文件重名的问题

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const formidable = require('formidable')
+const fs = require('fs')
 const log = require('./log.js')
 
 const config = require('./config.json')
@@ -9,79 +10,116 @@ var port = config.port || 8080
 // 默认路径 /upload
 const url = config.url || '/upload'
 // POST File Name
-const fileName = config.fileName || "file"
+const fileName = config.fileName || 'file'
 // Response URL Path
-const urlPath = config.urlPath || "url"
+const urlPath = config.urlPath || 'url'
 
-var tasks = []
+let timeoutId = null
+
+/**
+ * 为文件加上扩展名
+ *
+ * @param {String} filePath 文件路径
+ * @param {String} ext 文件扩展名
+ */
+function addExtForFile(filePath, ext) {
+    let newFilePath = filePath + '.' + ext
+    // 同步修改文件名字
+    fs.renameSync(filePath, newFilePath)
+    return newFilePath
+}
+
+var filePathArray = []
+let responseArray = []
+
 function addTask(file, response) {
-  tasks.push({
-    "file":file,
-    "response":response
-  })
+    let filePath = file.path
+    let fileName = file.name
 
-  // 第一个任务手动执行
-  if(tasks.length==1) {
-    handleTask()
-  }
+    let extension = fileName.substring(fileName.lastIndexOf('.') + 1)
+    let filePathHasExt = filePath.lastIndexOf('.' + extension) != -1
+    if (!filePathHasExt) {
+        // 如果文件路径没有扩展名，上传到图床会乱码，这里改名，加上扩展名
+        filePath = addExtForFile(filePath, extension)
+    }
+
+    filePathArray.push(filePath)
+    responseArray.push(response)
+
+    log.info('file count:' + filePathArray.length)
+
+    if (timeoutId) {
+        clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+        // 延迟上传，1秒之内所有的请求都会合并到一次请求中
+        handleTask()
+    }, 1000)
 }
 
 function handleTask() {
-  if(tasks.length) {
-    let file = tasks[0]["file"]
-    let response = tasks[0]["response"]
-    uploadImage(file).then((result)=>{
-      // 处理结果
-      if (result) {
-        response.writeHead(200, {"Content-Type": "text/json"})
-        let res = {status:'success'}
-        res[urlPath] = result
-        response.write(JSON.stringify(res))
-      } else {
-        response.writeHead(500, {"Content-Type": "text/json"})
-        response.write(JSON.stringify({
-          status:'false'
-        }))
-      }
-      response.end()
+    uploadImage(filePathArray).then((resultList) => {
+        // 处理结果
+        if (resultList) {
+            for (let index = 0; index < resultList.length; index++) {
+                const element = resultList[index]
+                const response = responseArray[index]
+                response.writeHead(200, { 'Content-Type': 'text/json' })
+                let res = { status: 'success' }
+                res[urlPath] = element
+                response.write(JSON.stringify(res))
+                response.end()
+            }
+        } else {
+            for (let index = 0; index < responseArray.length; index++) {
+                const response = responseArray[index]
+                response.writeHead(500, { 'Content-Type': 'text/json' })
+                response.write(
+                    JSON.stringify({
+                        status: 'false',
+                    })
+                )
+                response.end()
+            }
+        }
 
-      // 第一个任务退出
-      tasks.shift()
-      // 自动执行队列中的任务
-      handleTask()
+        tasks = []
+        filePathArray = []
+        responseArray = []
     })
-  }
 }
 
 // create server, listen to port
 http.createServer(function (req, res) {
-  if (req.url == url && req.method == 'POST') {
-    var form = new formidable.IncomingForm()
-    form.parse(req, function (err, fields, files) {
-      if(err) {
-        res.send(err)
-        return
-      }
-      let file = files[fileName]
+    if (req.url == url && req.method == 'POST') {
+        var form = new formidable.IncomingForm()
+        form.parse(req, function (err, fields, files) {
+            if (err) {
+                res.send(err)
+                return
+            }
+            let file = files[fileName]
 
-      if (!file) {
-        res.writeHead(500, {"Content-Type": "text/json"})
-        log.error('no file found')
-        res.write(JSON.stringify({
-          status: 'false',
-          info: 'plz upload a pic'
-        }))
+            if (!file) {
+                res.writeHead(500, { 'Content-Type': 'text/json' })
+                log.error('no file found')
+                res.write(
+                    JSON.stringify({
+                        status: 'false',
+                        info: 'plz upload a pic',
+                    })
+                )
+                res.end()
+                return
+            }
+
+            addTask(file, res)
+        })
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' })
+        res.write('The URL Path is ' + url)
         res.end()
-        return
-      }
-
-      addTask(file, res)
-    })
-  } else {
-      res.writeHead(404, {"Content-Type": "text/plain"})
-      res.write('The URL Path is ' + url)
-      res.end()
-  }
+    }
 }).listen(port)
 
 // start up server
--- picgo.js
+++ picgo.js
@@ -1,59 +1,37 @@
 const axios = require('axios')
-const fs = require('fs')
 const log = require('./log.js')
-
-/**
- * 为文件加上扩展名
- *
- * @param {String} filePath 文件路径
- * @param {String} ext 文件扩展名
- */
-function addExtForFile(filePath, ext) {
-    let newFilePath = filePath + '.' + ext
-    // 同步修改文件名字
-    fs.renameSync(filePath, newFilePath)
-    return newFilePath
-}
 
 /**
  * @param  {[String]} file {picture path}
  * @return {[String]} {picture pid}
  */
-async function uploadImage(file) {
+async function uploadImage(filePathArray) {
     try {
         // PicGo Server地址
-        let imageUrl = 'http://127.0.0.1:36677/upload'
+        let picGoServer = 'http://127.0.0.1:36677/upload'
 
-        let filePath = file.path
-        let fileName = file.name
-
-        let extension = fileName.substring(fileName.lastIndexOf('.') + 1)
-        let filePathHasExt = filePath.lastIndexOf('.' + extension) != -1
-        if (!filePathHasExt) {
-            // 如果文件路径没有扩展名，上传到图床会乱码，这里改名，加上扩展名
-            filePath = addExtForFile(filePath, extension)
-        }
-
-        log.info("开始上传文件：" + filePath)
-
+        log.info("开始上传文件：总共" + filePathArray.length + "个")
+        log.debug(filePathArray.join(","))
+        
         let body = {
-            list: [filePath],
+            list: filePathArray,
         }
-        let upImgResp = await axios.post(imageUrl, body, {
+        let upImgResp = await axios.post(picGoServer, body, {
             headers: {
                 'Content-Type': 'application/json; charset=utf-8',
             },
         })
-        imgUrl = upImgResp.data['result'][0]
-        if (imgUrl) {
-            log.info('success upload a pic to: ' + imgUrl)
-            return imgUrl
+        let imgUrlList = upImgResp.data['result']
+        if (imgUrlList) {
+            log.info('success upload ' + imgUrlList.length + ' pic')
+            return imgUrlList
         } else {
-            throw 'no img url '
+            return null
         }
     } catch (e) {
         log.error('upload failed with error: ' + e)
-        throw 'no img url '
+        // throw 'no img url '
+        return null
     }
 }
 
